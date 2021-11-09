55164d40e7d44c04de622cb8a985a19e5d9fcc70
Fagner de Oliveira Bernardo <fagner.oliveira@dtidigital.com.br>
2021-11-09 11:55:19 -0300

    Implementação da arquitetura do frontend - falta criar abstrações melhores para a parte de roteamento.

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -14,7 +14,6 @@ const rotasAutenticadas = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-
             <Route path="/about" element={<About />} />
         </Routes>
     );
