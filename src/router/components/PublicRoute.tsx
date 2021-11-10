407ad8b24698db744c2ee567e7e62410e0fd1263
Fagner de Oliveira Bernardo <fagner.oliveira@dtidigital.com.br>
2021-11-10 12:01:40 -0300

    Refatoração de código desnecessario

--- src/router/components/PublicRoute.tsx
+++ src/router/components/PublicRoute.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 
-interface PublicRouteProps {
-    component: React.FC;
-    path: string;
-}
+/*TODO: Analisar a necessidade dessa abstração de rotas publicas e privadas através deste componente.*/
 
-const PublicRoute: React.FC<PublicRouteProps> = ({ component: Component, path }) => {
-    // const render = () => <Component />;
+// import React from 'react';
+// import { Route } from 'react-router-dom';
 
-    // return <Route path={path} render={render} />;
-    return (
-        <div></div>
-    );
-};
+// interface PublicRouteProps {
+//     component: React.FC;
+//     path: string;
+// }
 
-export default PublicRoute;
+// const PublicRoute: React.FC<PublicRouteProps> = ({ component: Component, path }) => {
+//     const render = () => <Component />;
+//     return <Route path={path} render={render} />;
+// };
+
+// export default PublicRoute;
--- src/router/components/ScrollToTop.tsx
+++ src/router/components/ScrollToTop.tsx
@@ -1,19 +1,5 @@
 import React, { useEffect } from 'react';
-
 import { useLocation } from 'react-router-dom';
-// import { withRouter } from "react-router";
-
-// function ScrollToTop() {
-//     const { pathname } = useLocation();
-    
-//     useEffect(() => {
-//         window.scrollTo(0, 0);
-//     }, [pathname]);
-
-//     return null;
-// }
-
-// export default withRouter(ScrollToTop);
 
 const ScrollToTop: React.FC = () => {
 
@@ -26,4 +12,4 @@ const ScrollToTop: React.FC = () => {
     return null;
 };
 
-export default ScrollToTop
+export default ScrollToTop;
