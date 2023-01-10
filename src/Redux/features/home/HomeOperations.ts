cd35bfded1a4afe4524a30a0b6b3c9cdbd3d8e97
Fagner de Oliveira Bernardo <fagner.oliveira@dtidigital.com.br>
2023-01-10 12:32:17 -0300

    ...

--- src/Redux/features/home/HomeOperations.ts
+++ src/Redux/features/home/HomeOperations.ts
@@ -1,7 +1,8 @@
 import { Dispatch } from 'redux';
 import { ReduxDataStatus } from '../../../common/enums/redux/ReduxDataStatus';
-import { IHomeModel } from '../../../models/home/HomeModel';
 import { homeActions } from './HomeActions';
+
+import { IHomeModel } from '../../../models/home/HomeModel';
 
 export const setHomeRedux = (homeModel: IHomeModel) => (dispatch: Dispatch) => {
     dispatch(homeActions.setHomeRedux(homeModel));
