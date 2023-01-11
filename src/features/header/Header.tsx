c0fa20e67b35c050745ec2a85a71207290b8fd4a
Fagner de Oliveira Bernardo <fagner.oliveira@dtidigital.com.br>
2023-01-11 16:48:49 -0300

    Refatorando objeto de roteamento

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -4,12 +4,17 @@ import { useNavigate } from "react-router-dom";
 import { RotasFeatures } from "../../common/enums/rotas/RotasExemplo";
 import styles from './Header.module.css';
 
+interface IInformacaoRota {
+    Nome: string;
+    Rota: RotasFeatures
+}
+
 const Header: React.FC = () => {
 
-    const rotasDaAplicacao: Array<RotasFeatures> = [
-        RotasFeatures.Index,
-        RotasFeatures.Home,
-        RotasFeatures.About
+    const rotasDaAplicacao: Array<IInformacaoRota> = [
+        {Nome: 'Index', Rota: RotasFeatures.Index},
+        {Nome: 'Home', Rota: RotasFeatures.Home},
+        {Nome: 'About', Rota: RotasFeatures.About}
     ];
 
     const navigate = useNavigate();
@@ -21,10 +26,10 @@ const Header: React.FC = () => {
         <header className={styles['pageContainer']}>
             <div className={styles['containerItensMenu']}>
                 {rotasDaAplicacao.map((rota) => (
-                    <button key={rota}
+                    <button key={rota.Nome}
                         className={styles['botao']}
-                        onClick={() => { handleNavigate(rota) }}>
-                        Index
+                        onClick={() => { handleNavigate(rota.Rota) }}>
+                        {rota.Nome}
                     </button>
                 ))}
             </div>
