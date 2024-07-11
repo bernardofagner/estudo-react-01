d7d7e37259c9e4f025f893b3d65320c0e2d32a1b
Fagner de Oliveira Bernardo <bernardo.fagner@hotmail.com>
2024-07-11 18:10:36 -0300

    Resolvendo dependencias

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import './App.css';
 
 import { Header } from './features/header/Header';
 import { Router } from './router/Router';
-import { Footer } from './features/footer/Footer';
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <Header />
             <Router />
-            <Footer />
         </div>
     );
 }
